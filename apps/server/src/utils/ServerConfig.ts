d1484b12be6707b12784fd7fcfa34e433dd578da
lsshuai <302931504@qq.com>
2024-04-30 18:37:28 +0800

    feat: 修改server配置路径

--- apps/server/src/utils/ServerConfig.ts
+++ apps/server/src/utils/ServerConfig.ts
@@ -18,12 +18,10 @@ export class ServerConfig<T extends object> {
 
     private load() {
         const env = process.env.NODE_ENV === 'production' ? 'prod' : 'local';
-        const ymlFile: string = path.join(process.cwd(), `./config/${env}.yml`);
-
+        const ymlFile: string = path.resolve(process.cwd(), `../../../runtime/config/${env}.yml`);
         if (fs.existsSync(ymlFile)) {
             try {
                 const configs = yaml.load(fs.readFileSync(ymlFile, 'utf-8'));
-
                 this.configs = defaults(configs, this.configs);
             } catch (e) {
                 console.error(e.message);
