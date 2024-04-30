8ecfd2bce3afd634b2fea13022260442a7c36e19
lsshuai <302931504@qq.com>
2024-04-30 19:34:12 +0800

    feat: add app run script

---
+++ apps/web/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: 'app',
+            script: 'pnpm',
+            args: 'start',
+            watch: true,
+            env: {
+                PORT: 3000,
+                NODE_ENV: 'production',
+            },
+        }
+    ],
+};
