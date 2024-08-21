4070308586b1c4a9f1bada65c28eb97ea1b878d8
lsshuai <302931504@qq.com>
2024-08-21 16:59:08 +0800

    fix: fix typeorm entities path

--- apps/server/src/common/database.ts
+++ apps/server/src/common/database.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     password: database.password,
     database: database.database,
     port: 3306,
-    entities: [path.join(__dirname, '../entity/*.ts')],
+    entities: [path.join(__dirname, '../entity/*.{ts,js}')],
     synchronize: true,
 });
 
